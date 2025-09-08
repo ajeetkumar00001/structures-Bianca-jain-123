d7fc3f42002ecf36423a81efda2e1d4850bcf270
ajeetkumar00001 <mc250041002@iiti.ac.in>
2025-09-08 16:30:23 +0530

    Update question1.cpp

--- question1.cpp
+++ question1.cpp
@@ -4,8 +4,14 @@
 
 using namespace std;
 
-// TODO: Write the Book struct here
-// struct Book { ... };
+// Define Book struct
+struct Book {
+    char title[100];
+    char author[100];
+    int year;
+    int pages;
+    char isbn[30];
+};
 
 void print_book(int index, const char* title, const char* author, int year, int pages, const char* isbn) {
     cout << "Book[" << index << "]: "
@@ -22,7 +28,7 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    ifstream input(argv[1]);
+    istream& input = cin;
     if (!input) {
         cerr << "Error opening " << argv[1] << endl;
         return 1;
@@ -30,16 +36,27 @@ int main(int argc, char* argv[]) {
 
     int num_books;
     input >> num_books;
-    input.ignore(); // skip newline
+    input.ignore();
 
-    // TODO: Create array of Book
-    // Book books[10];
+    if (num_books > 10) {
+        cerr << "Error: maximum 10 books supported." << endl;
+        return 1;
+    }
 
-    // TODO: Read books from input
-    // for (int i = 0; i < num_books; i++) { ... }
+    Book books[10];
 
-    // TODO: Print books using print_book
-    // for (int i = 0; i < num_books; i++) { ... }
+
+        input.getline(books[i].author, 100);
+        input >> books[i].year;
+        input >> books[i].pages;
+        input.ignore();
+        input.getline(books[i].isbn, 30);
+    }
+
+    for (int i = 0; i < num_books; ++i) {
+        print_book(i, books[i].title, books[i].author, books[i].year, books[i].pages, books[i].isbn);
+    }
 
     return 0;
 }
+
